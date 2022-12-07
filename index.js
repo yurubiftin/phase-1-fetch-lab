d2d161e5990d21b1703cbd0f50fa447421b07704
yurubahmed <yurubiftin2002@gmail.com>
2022-12-07 12:31:25 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(response => response.json() )
+  .then(data => renderBooks(data) )
 }
 
 function renderBooks(books) {
@@ -15,3 +17,7 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
+  
+  
